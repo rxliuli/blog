0c5133110a5fab96755c94e0aee4bc1407521018
rxliuli <rxliuli@gmail.com>
2021-01-06 21:18:40 +0800

    更新自动 commit 脚本，使用更加直观的时间格式

--- scripts/commit.js
+++ scripts/commit.js
@@ -1,9 +1,10 @@
 const simpleGit = require('simple-git')
 const git = simpleGit()
+const { DateTime } = require('luxon')
 
 async function commit() {
   await git.add('.')
-  await git.commit(`更新 ${new Date().toLocaleString()}`)
+  await git.commit(`更新 ${DateTime.local().toFormat('yyyy-MM-dd HH:mm:ss')}`)
 }
 
 commit()
