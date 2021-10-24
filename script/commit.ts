00c060048b50481bf4c144166669379736dc5f9f
rxliuli <rxliuli@gmail.com>
2021-10-24 20:08:03 +0800

    chore: 迁移至 pnpm，删除 gulp，升级所有依赖修复语法高亮的错误

--- script/commit.ts
+++ script/commit.ts
@@ -1,9 +1,11 @@
 import simpleGit from 'simple-git'
 import { DateTime } from 'luxon'
 
-export async function commit() {
+async function commit() {
   const git = simpleGit()
   await git.add('.')
   await git.commit(`更新 ${DateTime.local().toFormat('yyyy-MM-dd HH:mm:ss')}`)
   await git.push()
 }
+
+commit()
