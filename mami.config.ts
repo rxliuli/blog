3fe6b5dd18afb1aa7a72121e7b8087b3a2c3314e
rxliuli <rxliuli@gmail.com>
2022-11-12 00:29:15 +0800

    chore: update mami

--- mami.config.ts
+++ mami.config.ts
@@ -4,6 +4,6 @@ import * as joplin from '@mami/plugin-joplin'
 import json from './.joplin-blog.json'
 
 export default defineConfig({
-  input: [joplin.input(json), ],
-  output:[hexo.output()]
+  input: [joplin.input(json)],
+  output: [hexo.output()],
 })
