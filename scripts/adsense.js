f087c69e223b60add1090151e5b8440f1da59538
rxliuli <rxliuli@gmail.com>
2023-10-21 22:45:55 +0900

    chore: add amp ads

---
+++ scripts/adsense.js
@@ -0,0 +1,8 @@
+hexo.extend.injector.register(
+  'head_end',
+  '<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>',
+)
+hexo.extend.injector.register(
+  'body_begin',
+  '<amp-auto-ads type="adsense" data-ad-client="ca-pub-9997051001110405"></amp-auto-ads>',
+)
