f16ea6c470e99642764699e51e9edcb3e977c7a7
rxliuli <rxliuli@gmail.com>
2023-10-22 12:48:59 +0900

    chore: 调整为仅为文章页面添加广告

--- scripts/adsense.js
+++ scripts/adsense.js
@@ -1,8 +1,5 @@
 hexo.extend.injector.register(
   'head_end',
-  '<script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>',
-)
-hexo.extend.injector.register(
-  'body_begin',
-  '<amp-auto-ads type="adsense" data-ad-client="ca-pub-9997051001110405"></amp-auto-ads>',
+  '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9997051001110405" crossorigin="anonymous"></script>',
+  'post',
 )
