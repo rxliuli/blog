abc3ee1126388c42d05fe24e2c70e7faffe6abd3
rxliuli <rxliuli@gmail.com>
2025-07-30 16:14:02 +0800

    chore: temp remove ads

--- scripts/adsense.js
+++ scripts/adsense.js
@@ -1,5 +1,5 @@
-hexo.extend.injector.register(
-  'head_end',
-  '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9997051001110405" crossorigin="anonymous"></script>',
-  'post',
-)
+// hexo.extend.injector.register(
+//   'head_end',
+//   '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9997051001110405" crossorigin="anonymous"></script>',
+//   'post',
+// )
